90ffdfa0fac14c3c6a36f24bfbdf9a4c2a2576f1
Sharif Mohammad Rashed <86651937+Sharif33@users.noreply.github.com>
2021-12-19 15:54:12 +0600

    Update Blogs.js

--- src/components/pages/Blogs/Blogs.js
+++ src/components/pages/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className="bg-dark" style={{ paddingTop: "50px" }}>
-            <h2 className="p-4 text-danger">Blogs coming Soon.</h2>
+            <h2 className="p-4 text-danger">Blogs coming Soon...</h2>
         </div>
     );
 };
